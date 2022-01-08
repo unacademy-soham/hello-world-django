2928d156c97114d12014cd0b81dfd887e20b0c77
Soham Mukherjee <sohamantipr000@gmail.com>
2022-01-08 18:35:27 +0530

    Added celery and redis with our project

--- deploymentsdemo/settings.py
+++ deploymentsdemo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jprpb9&!&(8i4^xb8v(p9o3s=5&zr63zx*s&t6+^z=pty6e)-4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
